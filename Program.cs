5ec627dcb82501ba9be0f5f6c5eb741370166081
Jamoliddin <xjamoliddin27@gmail.com>
2021-10-06 22:14:41 +0500

    Lab2 uploaded

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using lesson1.Solutions;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main1()
+        {
+            var lab2 = new Lab2();
+            lab2.Problem1();
+            lab2.Problem2();
+            lab2.Problem3();
+            lab2.Problem4();
+            lab2.Problem5();
+            lab2.Problem6();
+            lab2.Problem7();
+            lab2.Problem8();
+            lab2.Problem9();
+        }
+        
+     
+     
+    }
+}
---
+++ README.dm
@@ -0,0 +1,2 @@
+# Bootcamp Lab Solutions
+- Lab2
