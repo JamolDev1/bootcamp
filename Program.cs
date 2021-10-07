a784ac804717fba422542c691fab94ff5f0222a3
Jamoliddin <xjamoliddin27@gmail.com>
2021-10-07 11:31:39 +0500

    Lab 5 uploaded

--- Program.cs
+++ Program.cs
@@ -44,6 +44,29 @@ namespace lesson1
             lab4.Problem10();
         }
      
+         static void Main()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem1();
+            lab5.Problem2();
+            lab5.Problem3();
+            lab5.Problem4();
+            lab5.Problem5();
+            lab5.Problem6();
+            lab5.Problem7();
+            lab5.Problem8();
+            lab5.Problem9();
+            lab5.Problem10();
+            lab5.Problem11();
+            lab5.Problem13();
+            lab5.Problem14();
+            lab5.Problem16();
+            lab5.Problem17();
+            lab5.Problem18();
+            lab5.Problem19();
+            lab5.Problem20();
+            lab5.Problem21();
+        }
      
     }
 }
